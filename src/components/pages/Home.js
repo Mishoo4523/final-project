b0117d56a15a59a947781e2f2b8518a6175f3161
Mishoo4523 <mishomurad@gmail.com>
2022-07-06 20:17:32 +0300

    add new Home.js Page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,7 @@ function Home() {
       <div className="card home-card">
 
         <h5>
-
+          Insta-SeEds
         </h5>
 
         <div className="card-image">
@@ -15,12 +15,54 @@ function Home() {
 
         </div>
         <div className="card-content">
+
+           <i class="material-icons" style={{color:"red"}}>favorite</i>
           <h6>Post Title</h6>
           <p>Welcome to the world of Insta-Seeds</p>
           <input tpe="text"  placeholder="Enter comment" />
 
           </div>
         </div>
+
+        <div className="card home-card">
+
+<h5>
+  Insta-SeEds
+</h5>
+
+<div className="card-image">
+  <img src="https://images.unsplash.com/photo-1509456592530-5d38e33f3fdd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXBwbGUlMjBzZWVkc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+
+</div>
+<div className="card-content">
+
+   <i class="material-icons" style={{color:"red"}}>favorite</i>
+  <h6>Post Title</h6>
+  <p>Welcome to the world of Insta-Seeds</p>
+  <input tpe="text"  placeholder="Enter comment" />
+
+  </div>
+</div>
+<div className="card home-card">
+
+<h5>
+  Insta-SeEds
+</h5>
+
+<div className="card-image">
+  <img src="https://images.unsplash.com/photo-1509456592530-5d38e33f3fdd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXBwbGUlMjBzZWVkc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+
+</div>
+<div className="card-content">
+
+   <i class="material-icons" style={{color:"red"}}>favorite</i>
+  <h6>Post Title</h6>
+  <p>Welcome to the world of Insta-Seeds</p>
+  <input tpe="text"  placeholder="Enter comment" />
+
+  </div>
+</div>
+
       </div>
       
     
