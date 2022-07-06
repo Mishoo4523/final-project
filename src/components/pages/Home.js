37a698ab73b2722f9934dda249ea37ebdbd2105b
Mishoo4523 <mishomurad@gmail.com>
2022-07-06 19:50:04 +0300

    fix bugs

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home-container">
+      <div className="card home-card">
+
+        <h5>
+
+        </h5>
+
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1509456592530-5d38e33f3fdd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXBwbGUlMjBzZWVkc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+
+        </div>
+        <div className="card-content">
+          <h6>Post Title</h6>
+          <p>Welcome to the world of Insta-Seeds</p>
+          <input tpe="text"  placeholder="Enter comment" />
+
+          </div>
+        </div>
+      </div>
+      
+    
   )
 }
 
